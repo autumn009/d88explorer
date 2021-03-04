24111025f22746279bdf51b6714f7034c6fc7306
Akira Kawamata <autumn@piedey.co.jp>
2021-03-04 11:46:12 +0900

    fix for shift

--- BrokenMOFatImageDump/BrokenMOFatImageDump/IPL.cs
+++ BrokenMOFatImageDump/BrokenMOFatImageDump/IPL.cs
@@ -28,14 +28,14 @@ namespace BrokenMOFatImageDump
             if (ipl.sector[0] != 0xeb) throw new ApplicationException("ipl.sector[0] must be 0xeb");
             if (ipl.sector[1] != 0x3c) throw new ApplicationException("ipl.sector[1] must be 0x3c");
             if (ipl.sector[2] != 0x90) throw new ApplicationException("ipl.sector[2] must be 0x90");
-            ipl.SectorLength = ipl.sector[0xb] + ipl.sector[0xc] << 8;
+            ipl.SectorLength = ipl.sector[0xb] + (ipl.sector[0xc] << 8);
             ipl.ClusterPerSector = ipl.sector[0xd];
-            ipl.FATStartSector = ipl.sector[0xe] + ipl.sector[0xf] << 8;
+            ipl.FATStartSector = ipl.sector[0xe] + (ipl.sector[0xf] << 8);
             ipl.NumberOfFATs = ipl.sector[0x10];
-            ipl.TotalRootDirectories = ipl.sector[0x11] + ipl.sector[0x12] << 8;
-            ipl.TotalLogocalSectors = ipl.sector[0x13] + ipl.sector[0x14] << 8;
+            ipl.TotalRootDirectories = ipl.sector[0x11] + (ipl.sector[0x12] << 8);
+            ipl.TotalLogocalSectors = ipl.sector[0x13] + (ipl.sector[0x14] << 8);
             ipl.MediaDescripter = ipl.sector[0x15];
-            ipl.TotalSectorsInOneFAT = ipl.sector[016] + ipl.sector[0x17] << 8;
+            ipl.TotalSectorsInOneFAT = ipl.sector[016] + (ipl.sector[0x17] << 8);
 
             if (Util.IsVerbose)
             {
