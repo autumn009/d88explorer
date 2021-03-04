349a3785f5c00b3fb1c5cada8feab2e35a28c41b
Akira Kawamata <autumn@piedey.co.jp>
2021-03-04 11:20:44 +0900

    read IPL

--- BrokenMOFatImageDump/BrokenMOFatImageDump/IPL.cs
+++ BrokenMOFatImageDump/BrokenMOFatImageDump/IPL.cs
@@ -29,10 +29,24 @@ namespace BrokenMOFatImageDump
             if (ipl.sector[1] != 0x3c) throw new ApplicationException("ipl.sector[1] must be 0x3c");
             if (ipl.sector[2] != 0x90) throw new ApplicationException("ipl.sector[2] must be 0x90");
             ipl.SectorLength = ipl.sector[0xb] + ipl.sector[0xc] << 8;
+            ipl.ClusterPerSector = ipl.sector[0xd];
+            ipl.FATStartSector = ipl.sector[0xe] + ipl.sector[0xf] << 8;
+            ipl.NumberOfFATs = ipl.sector[0x10];
+            ipl.TotalRootDirectories = ipl.sector[0x11] + ipl.sector[0x12] << 8;
+            ipl.TotalLogocalSectors = ipl.sector[0x13] + ipl.sector[0x14] << 8;
+            ipl.MediaDescripter = ipl.sector[0x15];
+            ipl.TotalSectorsInOneFAT = ipl.sector[016] + ipl.sector[0x17] << 8;
 
             if (Util.IsVerbose)
             {
                 Console.WriteLine($"SectorLength={ipl.SectorLength}");
+                Console.WriteLine($"ClusterPerSector={ipl.ClusterPerSector}");
+                Console.WriteLine($"FATStartSector={ipl.FATStartSector}");
+                Console.WriteLine($"NumberOfFATs={ipl.NumberOfFATs}");
+                Console.WriteLine($"TotalRootDirectories={ipl.TotalRootDirectories}");
+                Console.WriteLine($"TotalLogocalSectors={ipl.TotalLogocalSectors}");
+                Console.WriteLine($"MediaDescripter={ipl.MediaDescripter}");
+                Console.WriteLine($"TotalSectorsInOneFAT={ipl.TotalSectorsInOneFAT}");
             }
 
             return ipl;
