30f01af39b7025d898557968a035a00a4306a59a
Akira Kawamata <autumn@piedey.co.jp>
2021-03-04 10:12:02 +0900

    SectorLength

--- BrokenMOFatImageDump/BrokenMOFatImageDump/IPL.cs
+++ BrokenMOFatImageDump/BrokenMOFatImageDump/IPL.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -9,9 +10,33 @@ namespace BrokenMOFatImageDump
 {
     class IPL
     {
+        private byte[] sector;
+        public int SectorLength;
+        public int ClusterPerSector;
+        public int FATStartSector;
+        public int NumberOfFATs;
+        public int TotalRootDirectories;
+        public int TotalLogocalSectors;
+        public byte MediaDescripter;
+        public int TotalSectorsInOneFAT;
+
         internal static IPL Load(FileStream stream)
         {
-            return new IPL();
+            var ipl = new IPL();
+            ipl.sector = new byte[512];
+            stream.Read(ipl.sector, 0, ipl.sector.Length);
+            if (ipl.sector[0] != 0xeb) throw new ApplicationException("ipl.sector[0] must be 0xeb");
+            if (ipl.sector[1] != 0x3c) throw new ApplicationException("ipl.sector[1] must be 0x3c");
+            if (ipl.sector[2] != 0x90) throw new ApplicationException("ipl.sector[2] must be 0x90");
+            ipl.SectorLength = ipl.sector[0xb] + ipl.sector[0xc] << 8;
+
+            if (Util.IsVerbose)
+            {
+                Console.WriteLine($"SectorLength={ipl.SectorLength}");
+            }
+
+            return ipl;
+
         }
     }
 }
