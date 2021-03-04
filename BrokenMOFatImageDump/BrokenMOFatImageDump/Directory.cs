167747b97c9dd4621e11dbe71537c356dd25bd96
Akira Kawamata <autumn@piedey.co.jp>
2021-03-04 12:08:05 +0900

    read root dir

--- BrokenMOFatImageDump/BrokenMOFatImageDump/Directory.cs
+++ BrokenMOFatImageDump/BrokenMOFatImageDump/Directory.cs
@@ -7,11 +7,38 @@ using System.Threading.Tasks;
 
 namespace BrokenMOFatImageDump
 {
+    class DirEnt
+    {
+        internal const int DirEntSize = 32;
+
+    }
+
+
+
     class Directory
     {
+
+        internal DirEnt[] Entries;
         internal static Directory LoadRoot(FileStream stream, IPL ipl, FAT fat)
         {
-            return new Directory();
+            var dir = new Directory();
+            var list = new List<DirEnt>();
+            var size = ipl.TotalRootDirectories * DirEnt.DirEntSize;
+            byte[] all = new byte[size];
+            stream.Read(all, 0, all.Length);
+            for (int i = 0; i < size; i+= DirEnt.DirEntSize)
+            {
+                if (all[i] == 0) break;
+                if (all[i] == 0xe5) continue;
+                for (int j = 0; j < 11; j++)
+                {
+                    var c = all[i + j];
+                    if (c != 0) Console.Write($"{(char)c}");
+                }
+                Console.WriteLine();
+            }
+            dir.Entries = list.ToArray();
+            return dir;
         }
     }
 }
--- BrokenMOFatImageDump/BrokenMOFatImageDump/FAT.cs
+++ BrokenMOFatImageDump/BrokenMOFatImageDump/FAT.cs
@@ -9,9 +9,17 @@ namespace BrokenMOFatImageDump
 {
     class FAT
     {
+        private byte[] fatall;
         internal static FAT Load(FileStream stream, IPL ipl)
         {
-            return new FAT();
+            var fat = new FAT();
+            var fatBytes = ipl.SectorLength * ipl.TotalSectorsInOneFAT;
+            fat.fatall = new byte[fatBytes];
+            for (int i = 0; i < ipl.NumberOfFATs; i++)
+            {
+                stream.Read(fat.fatall, 0, fat.fatall.Length);
+            }
+            return fat;
         }
     }
 }
