76807d902c02ff6eb9c7569b9dbc91570d55cba1
Akira Kawamata <autumn@piedey.co.jp>
2021-03-06 19:09:02 +0900

    add skelton

---
+++ d88toSimpleImage/d88toSimpleImage/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace d88toSimpleImage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
