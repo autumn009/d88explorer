dd2cc8bb74dae70cf0fb92dc13cf3b88632e2bed
Akira Kawamata <autumn@piedey.co.jp>
2021-03-07 11:56:44 +0900

    add command line runner

---
+++ d88explorer/d88ExtractorForNecDiskBasic/ExpUtil.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace d88ExtractorForNecDiskBasic
+{
+    static class ExpUtil
+    {
+        private static string badCharacters = "\\/:*?\"<>|";
+        private static string[] badNames = {"con", "aux","prn","nul",
+            "com0",
+            "com1",
+            "com2",
+            "com3",
+            "com4",
+            "com5",
+            "com6",
+            "com7",
+            "com8",
+            "com9",
+            "lpt0",
+            "lpt1",
+            "lpt2",
+            "lpt3",
+            "lpt4",
+            "lpt5",
+            "lpt6",
+            "lpt7",
+            "lpt8",
+            "lpt9",
+        };
+
+        internal static string ConvertD88FileNameToHostFileName(string filename)
+        {
+            var body = Path.GetFileNameWithoutExtension(filename);
+            var ext = Path.GetExtension(filename);
+            var bodyLower = body.ToLower();
+            if (badNames.Any(c => c == bodyLower)) filename = body + "_" + ext;
+
+            var sb = new StringBuilder();
+            foreach (var item in filename)
+            {
+                if (badCharacters.Contains(item))
+                {
+                    sb.AppendFormat("%{0:X2}", (int)item);
+                }
+                else
+                {
+                    sb.Append(item);
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ d88explorer/d88ExtractorForNecDiskBasic/Program.cs
@@ -0,0 +1,36 @@
+﻿using d88lib;
+using System;
+using System.IO;
+
+namespace d88ExtractorForNecDiskBasic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("usage: d88ExtractorForNecDiskBasic SRC_FILES_WITH_WILDCARD");
+                return;
+            }
+            foreach (var pathWithWildCard in args)
+            {
+                if (pathWithWildCard == "-9") continue;
+                foreach (var fullpath in Directory.GetFiles(Path.GetDirectoryName(pathWithWildCard), Path.GetFileName(pathWithWildCard)))
+                {
+                    VDisk currentVDisk = new VDisk(File.ReadAllBytes(fullpath)); ;
+                    var outputDirectoryName = Path.ChangeExtension(fullpath, null);
+                    TempDir currentTempDir = new TempDir(outputDirectoryName);
+                    FileDetails[] currentImage = currentTempDir.CreateImage(currentVDisk);
+
+
+                }
+            }
+
+
+
+
+
+        }
+    }
+}
