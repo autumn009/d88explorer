e74e97ccc592c7a6c619c465a5aee71930970d29
Akira Kawamata <autumn@piedey.co.jp>
2023-03-30 15:07:16 +0900

    temp save

--- d88explorer/d88ExtractorForFat12/Program.cs
+++ d88explorer/d88ExtractorForFat12/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using d88ExtractorForFat12;
+using d88lib;
+
+if (args.Length == 0)
+{
+    Console.WriteLine("usage: d88ExtractorForFat12 SRC_FILES_WITH_WILDCARD");
+    return;
+}
+foreach (var pathWithWildCard in args)
+{
+    foreach (var fullpath in Directory.GetFiles(Path.GetDirectoryName(pathWithWildCard??""), Path.GetFileName(pathWithWildCard)))
+    {
+        VDisk currentVDisk = new VDisk(File.ReadAllBytes(fullpath)); ;
+        var outputDirectoryName = Path.ChangeExtension(fullpath, null);
+        TargetDir currentTempDir = new TargetDir(outputDirectoryName);
+        currentTempDir.CreateImage(currentVDisk);
+    }
+}
---
+++ d88explorer/d88ExtractorForFat12/TargetDir.cs
@@ -0,0 +1,24 @@
+﻿using d88lib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d88ExtractorForFat12
+{
+    internal class TargetDir
+    {
+        public TargetDir(string outputDirectoryName)
+        {
+            OutputDirectoryName = outputDirectoryName;
+        }
+
+        public string OutputDirectoryName { get; }
+
+        internal void CreateImage(VDisk currentVDisk)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
