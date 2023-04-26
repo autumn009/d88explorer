3cb013e2228cd17361422b1c32d217745c65c1b4
Akira Kawamata <autumn@piedey.co.jp>
2023-04-26 18:36:30 +0900

    available for output dir

--- d88explorer/d88ExtractorForNecDiskBasic/Program.cs
+++ d88explorer/d88ExtractorForNecDiskBasic/Program.cs
@@ -1,5 +1,6 @@
 ﻿using d88lib;
 using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace d88ExtractorForNecDiskBasic
@@ -10,27 +11,35 @@ namespace d88ExtractorForNecDiskBasic
         {
             if (args.Length == 0)
             {
-                Console.WriteLine("usage: d88ExtractorForNecDiskBasic SRC_FILES_WITH_WILDCARD");
+                Console.WriteLine("usage: d88ExtractorForNecDiskBasic SRC_FILES_WITH_WILDCARD [-d DST_DIR]");
                 return;
             }
+            var list = new List<string>();
+            string dst = null;
+            bool dFlag = false;
             foreach (var pathWithWildCard in args)
             {
+                if (dFlag)
+                {
+                    dst = pathWithWildCard;
+                    dFlag = false;
+                    continue;
+                }
                 if (pathWithWildCard == "-9") continue;
+                if (pathWithWildCard == "-d") dFlag = true;
+                else list.Add(pathWithWildCard);
+            }
+            foreach (var pathWithWildCard in list)
+            {
                 foreach (var fullpath in Directory.GetFiles(Path.GetDirectoryName(pathWithWildCard), Path.GetFileName(pathWithWildCard)))
                 {
-                    VDisk currentVDisk = new VDisk(File.ReadAllBytes(fullpath)); ;
-                    var outputDirectoryName = Path.ChangeExtension(fullpath, null);
+                    VDisk currentVDisk = new VDisk(File.ReadAllBytes(fullpath));
+                    var outputDirectoryName = Path.Combine(dst,Path.GetFileNameWithoutExtension(fullpath));
+                    if (dst == null) outputDirectoryName = Path.ChangeExtension(fullpath, null);
                     TempDir currentTempDir = new TempDir(outputDirectoryName);
-                    FileDetails[] currentImage = currentTempDir.CreateImage(currentVDisk);
-
-
+                    FileDetails[] dummyCurrentImage = currentTempDir.CreateImage(currentVDisk);
                 }
             }
-
-
-
-
-
         }
     }
 }
